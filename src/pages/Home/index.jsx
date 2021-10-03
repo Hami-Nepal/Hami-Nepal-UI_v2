25469478781780a509088a14d46c8a202ce8b8b6
ramankarki <ramankarki40@gmail.com>
2021-10-03 15:33:22 +0545

    added video

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
+import Header from '../../templates/Header';
+
+import './home.scss';
+
+import dummyVideo from './video.mp4';
+
 function Home() {
-  return <h1>Home</h1>;
+  const [isMuted, setMuted] = useState(true);
+
+  return (
+    <div className="Home">
+      <Header />
+      <video
+        className="Home__video"
+        src={dummyVideo}
+        preload="metadata"
+        autoPlay={true}
+        muted={isMuted}
+        loop="loop"
+      ></video>
+    </div>
+  );
 }
 
 export default Home;
