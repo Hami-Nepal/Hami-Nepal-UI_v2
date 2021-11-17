92ae7f296e8aa968f9c19e1337c27cc894f5d2db
kishor <kishorbhandari608@gmail.com>
2021-11-17 11:40:42 +0530

    resolved-login

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios"
 
-const token = JSON.parse(localStorage.getItem('userInfo')).token;
+const token = JSON.parse(localStorage.getItem("userInfo")).token
 
 export default axios.create({
-  baseURL: 'https://haminepal.herokuapp.com/api/v1',
+  baseURL: "https://haminepal.herokuapp.com/api/v1/",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   },
-});
+})
