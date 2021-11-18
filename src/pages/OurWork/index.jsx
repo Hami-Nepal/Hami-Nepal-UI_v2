e410ba3fbbb6479e0ebf0cb4078b4f681e16bfd1
raman karki <ramankarki40@gmail.com>
2021-11-18 19:22:14 +0545

    responsive hero section

--- src/pages/OurWork/index.jsx
+++ src/pages/OurWork/index.jsx
@@ -6,9 +6,13 @@ import Tab from '../../templates/Tab';
 import './ourwork.scss';
 
 const OurWork = () => {
-  const [types, setTypes] = useState([]);
-  const [activeType, setActiveType] = useState('');
-  const [activeStatus, setActiveStatus] = useState('');
+  const [eventTypes, setEventTypes] = useState([]);
+  const [activeEventType, setActiveEventType] = useState('');
+  const [activeEventStatus, setActiveEventStatus] = useState('ongoing');
+
+  const [causeTypes, setCauseTypes] = useState([]);
+  const [activeCauseType, setActiveCauseType] = useState('');
+  const [activeCauseStatus, setActiveCauseStatus] = useState('ongoing');
 
   useEffect(async () => {
     const promises = [API.get('event_type'), API.get('cause_type')];
@@ -17,37 +21,38 @@ const OurWork = () => {
     const eventTypesDest = event_types.data.data.Event_type_var.map(
       (obj) => obj.event_type
     );
+    setEventTypes(eventTypesDest);
+    setActiveEventType(eventTypesDest[0]);
+
     const causeTypesDest = cause_types.data.data.Cause_type_var.map(
       (obj) => obj.cause_type
     );
-
-    const mergedTypes = [...new Set([...eventTypesDest, ...causeTypesDest])];
-    setTypes(mergedTypes);
-    setActiveType(mergedTypes[0]);
-    setActiveStatus('ongoing');
+    setCauseTypes(causeTypesDest);
+    setActiveCauseType(causeTypesDest[0]);
   }, []);
 
   return (
-    <>
-      <header className="hero-section">
-        <div className="quote_contain container">
-          <h1 className="heading__title">Our Work</h1>
-          <p className="heading__description">
-            Pulling up the roots of poverty and planting the seeds of change.
-            Building relationships. Earning the right to be heard. Planning and
-            working alongside local leaders. Finding solutions to change the
-            future for kids and the next generation. Here's just some of what we
-            accomplished together in 2020.
-          </p>
-        </div>
-      </header>
+    <div>
+      <div className="ourWorkHero">
+        <h1 className="ourWorkHero__heading">Our Work</h1>
+        <p className="ourWorkHero__desc">
+          Pulling up the roots of poverty and planting the seeds of change.
+          Building relationships. Earning the right to be heard. Planning and
+          working alongside local leaders. Finding solutions to change the
+          future for kids and the next generation. Here's just some of what we
+          accomplished together in 2020.
+        </p>
+      </div>
+
+      <h2>Causes</h2>
 
       <Tab
-        types={types}
-        setActiveStatus={setActiveStatus}
-        setActiveType={setActiveType}
+        types={eventTypes}
+        setActiveStatus={setActiveEventStatus}
+        setActiveType={setActiveEventType}
       />
-    </>
+    </div>
   );
 };
+
 export default OurWork;
