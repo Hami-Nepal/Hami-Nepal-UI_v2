3966217994ad446f5b2855ea779a6924837a4153
raman karki <ramankarki40@gmail.com>
2021-11-18 21:50:55 +0545

    simple card

---
+++ src/components/CauseEventCard/index.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import Button from '../../components/Button';
+
+import './causeEventCard.scss';
+
+const CauseEventCard = ({ card, href }) => {
+  console.log('ma card data herdai xu', card);
+  return (
+    <div className="causeEventCard">
+      <Link to={href}>
+        <p className="causeEventCard__title">{card.name}</p>
+        <img src={card.photos[0]} alt={card.name} />
+        <p className="causeEventCard__summary">{card.summary}</p>
+        <Button text="Donate" variant="official" />
+      </Link>
+    </div>
+  );
+};
+
+export default CauseEventCard;
