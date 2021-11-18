b812d885eb02c3727eef8072775ef536b0daed78
raman karki <ramankarki40@gmail.com>
2021-11-18 22:03:53 +0545

    update link path

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import Button from "../../components/Button"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
 
-import dummyVideo from "./video.mp4"
-import "./hero.scss"
+import dummyVideo from './video.mp4';
+import './hero.scss';
 
 const Hero = () => {
-  const [isMuted, setMuted] = useState(true)
-  const [show, setShow] = useState("false")
+  const [isMuted, setMuted] = useState(true);
+  const [show, setShow] = useState('false');
 
-  const handleClose = () => setShow(false)
+  const handleClose = () => setShow(false);
 
   return (
     <div className="main">
@@ -27,11 +27,11 @@ const Hero = () => {
           <h1 className="slogan">For the people by the people</h1>
           <Button text="Donate" variant="transparent" />
         </div>
-        <Link onChange={handleClose} to="/common-landing-page">
+        <Link onChange={handleClose} to="/our-work">
           <button className="lowerContent__ourwork">Our Work</button>
         </Link>
       </div>
     </div>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
