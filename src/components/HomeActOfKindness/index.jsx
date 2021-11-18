9999914b83ae0dd5c3859f4c788515e71143a303
raman karki <ramankarki40@gmail.com>
2021-11-18 17:31:16 +0545

    fix styl

--- src/components/HomeActOfKindness/index.jsx
+++ src/components/HomeActOfKindness/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import API from '../../utils/API';
 
 import './style.scss';
@@ -21,9 +22,11 @@ const HomeActOfKindness = () => {
       <div className="homeActOfKindness__cards">
         {cards.map((card) => (
           <div className="card" key={card._id}>
-            <p className="card__title">{card.title}</p>
-            <img src={card.photos[0]} alt={card.title} />
-            <p className="card__summary">{card.summary}</p>
+            <Link to={'/act-of-kindness/' + card._id}>
+              <p className="card__title">{card.title}</p>
+              <img src={card.photos[0]} alt={card.title} />
+              <p className="card__summary">{card.summary}</p>
+            </Link>
           </div>
         ))}
       </div>
