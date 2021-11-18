cce9f3ccd72a94485d990c2b4f37f586868a663d
raman karki <ramankarki40@gmail.com>
2021-11-18 21:50:18 +0545

    add label

--- src/templates/Tab/index.jsx
+++ src/templates/Tab/index.jsx
@@ -3,27 +3,33 @@ import './tab.scss';
 const Tab = ({ types, setActiveType, setActiveStatus }) => {
   return (
     <div className="tab">
-      <select
-        className="tab__select"
-        onChange={(e) => setActiveType(e.target.value)}
-      >
-        {types.map((type) => (
-          <option key={type} className="tab__option" value={type}>
-            {type}
+      <label className="tab__label">
+        <p>Type</p>
+        <select
+          className="tab__select"
+          onChange={(e) => setActiveType(e.target.value)}
+        >
+          {types.map((type) => (
+            <option key={type} className="tab__option" value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="tab__label">
+        <p>Status</p>
+        <select
+          className="tab__select"
+          onChange={(e) => setActiveStatus(e.target.value)}
+        >
+          <option className="tab__option" value="ongoing">
+            On going
           </option>
-        ))}
-      </select>
-      <select
-        className="tab__select"
-        onChange={(e) => setActiveStatus(e.target.value)}
-      >
-        <option className="tab__option" value="ongoing">
-          On going
-        </option>
-        <option className="tab__option" value="past">
-          Past
-        </option>
-      </select>
+          <option className="tab__option" value="past">
+            Past
+          </option>
+        </select>
+      </label>
     </div>
   );
 };
